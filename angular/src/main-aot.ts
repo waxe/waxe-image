115aed6bce3ec50cedbbeca048bf0d3145bebb33
Aurélien Matouillot <a.matouillot@gmail.com>
2017-10-04 07:43:16 +0200

    aot

---
+++ angular/src/main-aot.ts
@@ -0,0 +1,7 @@
+import 'polyfills';
+
+import { platformBrowser }    from '@angular/platform-browser';
+import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
+
+
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
