a0f07caff4e9647d9aeef9dbd85eab4e2ece8523
Aurélien Matouillot <a.matouillot@gmail.com>
2017-10-09 18:12:54 +0200

    Add search on filename and tags

--- angular/src/app/file/file-list.component.ts
+++ angular/src/app/file/file-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import { Observable } from 'rxjs/Rx';
 
 import { IFile } from './file';
 import { ITag } from '../tag/tag';
@@ -9,22 +12,60 @@ import { TagService } from '../tag/tag.service';
 @Component({
   template: `
   <div class="row">
+  <div class="col-sm-offset-2 col-sm-12">
+    <br>
+    <br>
+    <input class="form-control" placeholder="search..." #search>
+    <br>
+  </div>
     <div class="col-sm-3" *ngFor="let file of files">
       <file [file]="file"></file>
     </div>
   </div>`,
 })
-export class FileListComponent implements OnInit {
+export class FileListComponent implements AfterViewInit, OnInit {
   files: IFile[] = [];
+  allFiles: IFile[] = [];
 
-  constructor(private fileService: FileService, private tagService: TagService) {}
+  private inputValue: Observable<string>;
+
+  @ViewChild('search') input: ElementRef;
+
+  constructor(private fileService: FileService, public tagService: TagService) {}
+
+
+  ngAfterViewInit() {
+    this.inputValue = fromEvent(this.input.nativeElement, 'input', ($event) => $event.target.value);
+
+
+    this.inputValue.debounceTime(200).subscribe((value: string) => {
+      const re = new RegExp(value, 'gi');
+      this.files = this.allFiles.filter((file: IFile) => {
+        return this.fileMatch(re, file);
+      });
+    });
+  };
+
+  fileMatch(re: RegExp, file: IFile) {
+    if (re.test(file.path)) {
+      return true;
+    }
+
+    const tags: ITag[] = file.tags.filter((tag: ITag) => {
+      return re.test(tag.name);
+    })
+
+    if(tags.length) {
+      return true;
+    }
+  }
 
   ngOnInit() {
     this.tagService.getTags(true).subscribe((tags: ITag[]) => {});
 
     this.fileService.getFiles().then((files: IFile[]) => {
-      this.files= files;
+      this.allFiles = files;
+      this.files = files;
     });
   }
-
 }
