9d9a886e304f3e3eb781fbc7ffbc14be48d9ff7f
Aurélien Matouillot <a.matouillot@gmail.com>
2017-10-30 08:02:05 +0100

    Add group support in the API

--- waxe_image/views/files.py
+++ waxe_image/views/files.py
@@ -1,7 +1,28 @@
 from pyramid.view import view_config, view_defaults
 import pyramid.httpexceptions as exc
 
-from ..models import File, Tag
+from ..models import File, Group, Tag
+
+
+@view_defaults(renderer='json')
+class GroupView(object):
+
+    def __init__(self, request):
+        self.request = request
+
+    @view_config(route_name='groups', request_method='GET')
+    def get(self):
+        query = self.request.dbsession.query(Group)
+        groups = query.all()
+        lis = []
+        for g in groups:
+            lis.append({
+                'id': g.group_id,
+                'name': g.name
+            })
+        return {
+            'groups': lis
+        }
 
 
 @view_defaults(renderer='json')
@@ -12,10 +33,13 @@ class FileView(object):
 
     @view_config(route_name='files', request_method='GET')
     def get(self):
-        query = self.request.dbsession.query(File)
-        files = query.all()
+        group_id = self.request.matchdict.get('group_id')
+
+        query = self.request.dbsession.query(Group)\
+                    .filter_by(group_id=group_id)
+        group = query.one()
         lis = []
-        for f in files:
+        for f in group.files:
             lis.append({
                 'id': f.file_id,
                 'path': f.path,
@@ -41,6 +65,7 @@ class FileView(object):
 
 
 def includeme(config):
-    config.add_route('files', '/api/files')
+    config.add_route('files', '/api/groups/{group_id}/files')
     config.add_route('files_tags', '/api/files/{file_id}/tags')
+    config.add_route('groups', '/api/groups')
     config.scan(__name__)
